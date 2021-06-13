b5389c09bfc8860a3c3ec7d9b628c88783dd90d1
JosephAr1 <jonielhet@yahoo.com>
2021-06-13 12:16:09 +1200

    Add project files.

---
+++ SwapNosWithoutTempVar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwapNosWithoutTempVar
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 10, b = 25;
+            Console.WriteLine("value of a is : "+a);
+            Console.WriteLine("value of b is : " + b);
+            Console.ReadLine();
+            Console.WriteLine("Press Any key to continue");
+            Console.ReadKey();
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine(" New value of a is : " + a);
+            Console.WriteLine(" New value of b is : " + b);
+            Console.ReadLine();
+
+        }
+    }
+}
